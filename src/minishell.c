3df17169eac60e24689db93b5db54b1cea5e2e5c
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-06-20 18:51:17 +0900

    add: signal 처리

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,12 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:18:10 by woonshin          #+#    #+#             */
-/*   Updated: 2024/06/19 18:07:37 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/06/20 18:28:03 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "execute.h"
 
 typedef struct s_redirection {
 	int		type;
@@ -18,34 +19,11 @@ typedef struct s_redirection {
 	t_redir	*next;
 }	t_redir;
 
-void	init_signal(void)
-{
-	struct termios	term;
-
-	tcgetattr(STDIN_FILENO, &term);
-	term.c_lflag &= ~(ECHOCTL);
-	tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	signal(SIGQUIT, SIG_IGN);
-}
-
-void	execute(t_redir *redir_list)
-{
-	t_redir	*cur;
-
-	cur = redir_list;
-	while (cur)
-	{
-		execute_heredoc();
-		if (cur->next)
-			cur = cur->next;
-	}
-}
-
 void	minishell(int argc, char **argv, char **envp)
 {
-	char	*input;
-	t_list	*env_list;
-	t_redir	*redir_list;
+	char		*input;
+	t_list		*env_list;
+	t_command	*command;
 
 	env_list = init_envp(envp);
 	while (1)
@@ -54,7 +32,7 @@ void	minishell(int argc, char **argv, char **envp)
 		if (input)
 		{
 			parse(input);
-			execute(redir_list);
+			execute(command);
 		}
 		else
 			break ;

007d1a9af4b53c8281e8c5bca1f7fe220278f035
Woongbi Shin <woonshin@c1r4s4.42seoul.kr>
2024-06-23 17:49:04 +0900

    updated: echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: woonshin <woonshin@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 01:28:45 by dakyo             #+#    #+#             */
-/*   Updated: 2024/06/23 17:19:18 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/06/23 17:47:28 by woonshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@ void	echo(t_ASTNode *node, t_io *io_handler)
 
 	i = 1;
 	option = 0;
-	if (node->cmd->argv && compare_str(node->cmd->argv[1], "-n", 2) == 0)
+	if (1 < node->cmd->argc && node->cmd->argv
+		&& compare_str(node->cmd->argv[1], "-n", 2) == 0)
 	{
 		i++;
 		option = 1;
 	}
-	while (node->cmd->argv[i])
+	while (1 < node->cmd->argc && node->cmd->argv[i])
 	{
 		write(io_handler->output_fd, node->cmd->argv[i],
 			ft_strlen(node->cmd->argv[i]));
